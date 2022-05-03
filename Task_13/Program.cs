377c1a3ee680f4a245495c9b2c061222bf0cf286
ElenaPrudnikova <elena8sen@yandex.ru>
2022-05-03 18:36:58 +0300

    add Task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,15 @@
+﻿int number = RandomInt(10, 1000); 
+Console.WriteLine(number);
+string stringNumber = Convert.ToString(number);
+
+int RandomInt(int arg1, int arg2)
+{
+    return new Random().Next(arg1, arg2);
+}
+
+if ((number/10 > 0) && (number/100 == 0))
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+Console.WriteLine("Третья цифра: " + $"{stringNumber[2]}");
